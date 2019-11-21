e3007261b497cd67a277129bc31ec0199f6a68ff
Dan Shriver <dshriver53@gmail.com>
2019-11-21 12:41:33 -0600

    Created goal description area

--- skills/lib/features/skills/presentation/pages/newSkillScreen.dart
+++ skills/lib/features/skills/presentation/pages/newSkillScreen.dart
@@ -21,11 +21,13 @@ class NewSkillScreen extends StatefulWidget {
 
 class _NewSkillScreenState extends State<NewSkillScreen> {
   NewSkillBloc _bloc;
+  bool _goalAdded;
 
   @override
   void initState() {
     super.initState();
     _bloc = locator<NewSkillBloc>();
+    _goalAdded = false;
   }
 
   TextEditingController _nameController = TextEditingController();
@@ -47,9 +49,8 @@ class _NewSkillScreenState extends State<NewSkillScreen> {
     // Navigator.of(context).pop();
   }
 
-  void _goToGoalEditor()
-  {
-    Navigator.of(context).push(MaterialPageRoute(builder: (context){
+  void _goToGoalEditor() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
       return GoalCreationScreen();
     }));
   }
@@ -80,6 +81,7 @@ class _NewSkillScreenState extends State<NewSkillScreen> {
                 decoration: InputDecoration(labelText: 'Source'),
               ),
             ),
+            _goalDescriptionArea(_goalAdded),
             Padding(
               padding: const EdgeInsetsDirectional.only(bottom: 8.0),
               child: ButtonBar(
@@ -153,8 +155,48 @@ class _NewSkillScreenState extends State<NewSkillScreen> {
     );
   }
 
+  String _translateGoalToString()
+  {
+    String goalString = "Goal: ";
+
+    return goalString;
+  }
+
+  Container _goalDescriptionArea(bool withGoal) {
+    String desc =
+        'Goal: 5 hrs from 11/15 to 11/21 sll laljjdl jlgjlj  jallj agjlasljl ';
+
+    Widget body;
+    if (withGoal) {
+      body = Container(
+        color: Colors.grey[100],
+        child: InkWell(
+          onTap: (){
+            _goToGoalEditor();
+          },
+          child: Padding(
+              padding: const EdgeInsets.only(top: 4, bottom: 4),
+              child: Text(
+                desc,
+                style: Theme.of(context).textTheme.subhead,
+                maxLines: 2,
+                textAlign: TextAlign.start,
+                overflow: TextOverflow.ellipsis,
+              )),
+        ),
+      );
+    } else {
+      body = Container();
+    }
+
+    return body;
+  }
+
   @override
   Widget build(BuildContext context) {
+    // TODO - testing/layout only
+    _goalAdded = true;
+
     return BlocProvider(
         builder: (_) => _bloc,
         child: Scaffold(
@@ -167,8 +209,8 @@ class _NewSkillScreenState extends State<NewSkillScreen> {
               builder: (context, state) {
                 Widget body;
                 if (state is EmptyNewSkillState) {
-                  body = _newSkillAreaBuilder();
-                  // body = _skillUpdateArea();
+                  // body = _newSkillAreaBuilder();
+                  body = _skillUpdateArea();
                 } else if (state is NewSkillInsertedState) {
                   body = _skillUpdateArea();
                 }
