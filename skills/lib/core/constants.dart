d2809322f35f0ff4ab2299eb4f4f3c78fc89b874
Dan Shriver <dshriver53@gmail.com>
2020-02-13 10:13:00 -0600

    Instruments string constants

--- skills/lib/core/constants.dart
+++ skills/lib/core/constants.dart
@@ -1,2 +1,48 @@
 const String CACHE_FAILURE_MESSAGE = 'Cache Failure';
 const String SERVER_FAILURE_MESSAGE = 'Server Failure';
+
+const GUITAR_CL = 'Guitar (Classical)';
+const GUITAR_AC = 'Guitar (Acoustic)';
+const GUITAR_EL = 'Guitar (Electric)';
+const MANDOLIN = 'Mandolin';
+const PIANO = 'Piano';
+const ORGAN = 'Organ';
+const VIOLIN = 'Violin';
+const VIOLA = 'Viola';
+const CELLO = 'Cello';
+const BASS = 'Bass';
+const BASS_G = 'Bass (Guitar)';
+const DOUBLE_BASS = 'Double Bass';
+const DRUMS = 'Drums';
+const PERC = 'Percussion';
+const TRUMPET = 'Trumpet';
+const SAX_T = 'Saxophone (tenor)';
+const SAX_A = 'Saxophone (alto)';
+const SAX_S = 'Saxophone (sopr)';
+const CLARINET = 'Clarinet';
+const OBOE = 'Oboe';
+const TROMB = 'Trombone';
+
+const INSTRUMENTS = [
+  GUITAR_CL,
+  GUITAR_AC,
+  GUITAR_EL,
+  MANDOLIN,
+  PIANO,
+  ORGAN,
+  VIOLIN,
+  VIOLA,
+  CELLO,
+  BASS,
+  BASS_G,
+  DOUBLE_BASS,
+  DRUMS,
+  PERC,
+  TRUMPET,
+  SAX_T,
+  SAX_A,
+  SAX_S,
+  CLARINET,
+  OBOE,
+  TROMB
+];
