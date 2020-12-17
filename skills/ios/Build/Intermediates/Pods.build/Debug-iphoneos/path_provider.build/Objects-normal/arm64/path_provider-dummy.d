3cdee5a1fe1e5561134c3d9b0737a328576d87b9
Dan Shriver <dshriver53@gmail.com>
2020-12-17 12:00:30 -0600

    flutter updates

--- skills/ios/Build/Intermediates/Pods.build/Debug-iphoneos/path_provider.build/Objects-normal/arm64/path_provider-dummy.d
+++ skills/ios/Build/Intermediates/Pods.build/Debug-iphoneos/path_provider.build/Objects-normal/arm64/path_provider-dummy.d
@@ -2,5 +2,5 @@ dependencies: \
   /Users/danshriver/Developer/Skills_flutter/skills/ios/Pods/Target\ Support\ Files/path_provider/path_provider-dummy.m \
   /Users/danshriver/Developer/Skills_flutter/skills/ios/Build/Intermediates/Pods.build/Debug-iphoneos/path_provider.build/module.modulemap \
   /Users/danshriver/Developer/Skills_flutter/skills/ios/Pods/Target\ Support\ Files/path_provider/path_provider-prefix.pch \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.1.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.1.sdk/usr/include/mach-o/dyld.modulemap
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.2.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.2.sdk/usr/include/mach-o/dyld.modulemap
---
+++ skills/ios/Build/Intermediates/Runner.build/Profile-iphoneos/Runner.build/Objects-normal/arm64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/danshriver/Developer/Skills_flutter/skills/ios/Build/Intermediates/Runner.build/Profile-iphoneos/Runner.build/DerivedSources/Runner_vers.c
