b75b8c855a6775aa3bab2e8438c8deddf7c4e1f2
dan461 <dshriver53@gmail.com>
2021-04-08 13:07:42 -0500

    Delete skills/lib/core directory

--- skills/lib/core/textStyles.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class TextStyles {
-  static TextStyle subtitleRedStyle =
-      TextStyle(color: Colors.red, fontSize: 14, fontWeight: FontWeight.w500);
-  static TextStyle subheadDisabled =
-      TextStyle(color: Colors.grey, fontSize: 16);
-}
